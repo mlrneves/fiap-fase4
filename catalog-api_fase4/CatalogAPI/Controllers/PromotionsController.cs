8fb2a85b008a849e4b749fd64bbe22ba9e185484
mlrneves <marciorodriguesneves@gmail.com>
2026-05-18 21:38:18 -0300

    live deploy test

--- catalog-api_fase4/CatalogAPI/Controllers/PromotionsController.cs
+++ catalog-api_fase4/CatalogAPI/Controllers/PromotionsController.cs
@@ -18,6 +18,19 @@ namespace FCGApi.Controllers
             _promotionService = promotionService;
         }
 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_promotionService.ObterTodosAtivosDto());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
         [HttpGet("{id:int}")]
         public IActionResult Get([FromRoute] int id)
         {
